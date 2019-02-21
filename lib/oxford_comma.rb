3fef38b5604b3c34d7fe0e229eaab5409993dfa2
Patrick Kim <pkim050@gmail.com>
2019-02-21 06:19:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,11 @@ def oxford_comma(array)
   arr = []
   array.each_with_index do |element, index|
     if index + 1 == array.size
-      arr << element.join("and ")
+      arr << " and "
+      arr << element
     else
-      arr << element.join(", ")
+      arr << element
+      arr << ", "
     end
   end
   arr
